14c6def09f20590ac5b07c0d95f4fd5b6edbaa45
Checha Andrey <chechaandrey23@gmail.com>
2021-12-28 14:22:01 +0200

    pre deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,49 @@ import {SequelizeModule} from "@nestjs/sequelize";
 import {GuestModule} from './guest/guest.module';
 import {FinalModule} from './final/final.module';
 
+let postgres_Config: any;
+if(process.env.DATABASE_URL) {
+	let match = (process.env.DATABASE_URL || '').match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+	match = match || [];
+	let dbname = match[5];
+	let username = match[1];
+	let password = match[2];
+	let host = match[3];
+	let port = match[4];
+
+	postgres_Config = {
+		host: host,
+		port: port,
+		username: username,
+		password: password,
+		database: dbname,
+
+		dialect: 'postgres',
+		protocol: 'postgres',
+		dialectOptions: {
+			//ssl: true
+			ssl: {
+				require: true,
+				rejectUnauthorized: false
+			}
+		},
+		autoLoadModels: true
+	};
+} else {
+	postgres_Config ={
+		dialect: 'postgres',
+		host: 'localhost',
+		port: 5432,
+		username: 'postgres',
+		password: 'password',
+		database: 'maps',
+		autoLoadModels: true
+	};
+}
+
 @Module({
 	imports: [
-		SequelizeModule.forRoot({
-			dialect: 'postgres',
-			host: 'localhost',
-			port: 5432,
-			username: 'postgres',
-			password: 'password',
-			database: 'maps',
-			autoLoadModels: true
-		}),
+		SequelizeModule.forRoot(postgres_Config),
 		GuestModule,
 		FinalModule
 	],
